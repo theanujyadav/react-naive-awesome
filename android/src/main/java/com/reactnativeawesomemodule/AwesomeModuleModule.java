4f88e3b9dccd75bc89b5a3674a1ffca05ac117b3
Anuj yadav <anuj@springtimesoftware.net>
2022-04-07 21:29:51 +0530

    chore: initial commit

---
+++ android/src/main/java/com/reactnativeawesomemodule/AwesomeModuleModule.java
@@ -0,0 +1,34 @@
+package com.reactnativeawesomemodule;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = AwesomeModuleModule.NAME)
+public class AwesomeModuleModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "AwesomeModule";
+
+    public AwesomeModuleModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
